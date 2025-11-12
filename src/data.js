2d1e3448328d02d19412047fe32e862bfa060b09
Anina <Anina.Spaeth@htwg-konstanz.de>
2025-11-12 17:02:55 +0100

    add data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const torten = [
+    {
+        id: 1,
+        name: 'Lotus-Karamell',
+        beschreibung: 'Kuchen mit Lotus und Karamell',
+        preis: 39.90,
+        bildUrl: './assets/images/Kuchen_Lotus-caramell.png',
+    },
+    {
+        id: 2,
+        name: 'Schoko-Ganache',
+        beschreibung: 'Schokoladen kuchen',
+        preis: 39.90,
+        bildUrl: './assets/images/Kuchen_Schokolade.png',
+    },
+    {
+        id: 3,
+        name: 'Pistazien-Himbeer',
+        beschreibung: '...',
+        preis: 39.90,
+        bildUrl: './assets/images/Kuchen_pistazien-Himbeer.png',
+    },
+    {
+        id: 4,
+        name: 'Beeren-Sahne',
+        beschreibung: '...',
+        preis: 39.90,
+        bildUrl: './assets/images/Kuchen_beeren-Sahne.png',
+    }
+]
