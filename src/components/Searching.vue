0882a53ec5d6c914667eb065a787eeba7f5a851d
al871boy <ale871boy@htwg-konstanz.de>
2025-12-03 18:25:13 +0100

    button

--- src/components/Searching.vue
+++ src/components/Searching.vue
@@ -36,23 +36,25 @@ function onReset() {
 </script>
 <template>
   <div class="custom-search">
+    <div class="search-input-wrapper">
       <img src="/src/assets/images/magnifying-glass.svg" alt="Suchleiste" class="icon search-icon">
     <input type="text" placeholder="Produktname suchen ..." aria-label="Produktname suchen ..."   v-model="searchName"
            @keyup.enter="onSearch" />
-    <button class="filter-btn" @click="showFilter = !showFilter" aria-label="Filter">
+    <button class="filter-toogle" @click="showFilter = !showFilter" aria-label="Filter">
         <img src="/src/assets/images/sliders-horizontal.svg" alt="filter"  /> 
     </button>
+  </div>
                        <div v-if="showFilter" class="filter-dropdown">
           <select v-model="selectedCategory" @change="onSearch">
               <option value="">Alle Kategorien</option>
               <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
           </select>
-          <button id="search" @click="onSearch">Suchen</button>
-          <button id= "Reset" @click="onReset">Reset</button>
+          <button class="btn search-btn" @click="onSearch">Suchen</button>
+          <button id= "btn reset-btn" @click="onReset">Reset</button>
                         
   
   </div>
-  </div>
+</div>
 
 </template>
 
@@ -64,42 +66,55 @@ function onReset() {
   background-color: white;
 }
 .custom-search {
-  display: flex;
-  align-items: center;
+  width:100%;
+  max-width: 600px;
+  margin: 1rem auto;
+ 
   background-color: #BFA3B2;
-  padding: 8px 12px;
-  border-radius: 12px;
-  width: 100%;
+  border-radius:12px; 
+  padding: 0.5rem; 
+ display : flex; 
+  flex-direction:column; 
 }
 
-.custom-search input {
+.search-input-wrapper input {
+  flex:1;
   border: none;
-  background: transparent;
-  outline: none;
+ outline:none;
   flex: 1;
-  padding: 4px 8px;
+  padding: 0.5 rem;
+  border-radius:8px; 
   font-size: 14px;
-  color: #333;
+  
 }
-
+.filter-toggle{ background: transparent;
+border:none;
+cursor:pointer;
+margin-left: 0.5rem;}
 .icon {
   width: 20px;
   height: 20px;
-  flex-shrink: 0;
+  margin-right: 0.5rem;
+}
+.filter-drogdown{ margin-top:0.5rem;
+display:flex;
+flex-direction:column;
+gap:0.5rem;}
+.filter-buttons{ 
+  display:flex;
+  gap: 0.5rem;
 }
 
-.search-icon {
-  margin-right: 8px;
-}
-
-.filter-btn {
+.filter-buttons .btn {
   border: none;
-  background: transparent;
+  padding:0.5rem;
+  border-radius:8px;
   cursor: pointer;
-  margin-left: 8px;
-  display: flex;
-  align-items: center;
+ 
 }
+search-btn{ background-color: #6c4a5f;
+color:white;}
+reset-btn{ background-color: #ddd; }
 </style>
 
 
